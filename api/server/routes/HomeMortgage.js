bd7e7d3dfc14842ace44cfe45a9663e0aa0bc689
Joey Wong <joeywong@joeys-mbp.lan>
2019-11-17 20:10:28 -0500

    Added getall and getbyid

--- api/server/routes/HomeMortgage.js
+++ api/server/routes/HomeMortgage.js
@@ -4,6 +4,8 @@ import MortgageController from '../controller/home_mortgage/MortgageController';
 const router = Router();
 
 router.get('/calculate', MortgageController.getMortgageCalculation);
+router.get('/calculations', MortgageController.getAllMortgageCalculations)
+router.get('/calculations/:id', MortgageController.getMortgageCalculationById)
 router.post('/calculate', MortgageController.saveMortgageCalculation)
 
 export default router;
